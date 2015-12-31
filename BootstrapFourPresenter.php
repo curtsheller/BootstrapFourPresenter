17a9447a596d76d69e7788c2e72f63628c8b0e26
Curt Sheller <curt@curtsheller.com>
2015-12-30 20:40:58 -0500

    Larval 5.1> Bootstrap Four pagination presenter. Also used FontAwesome (not required)

---
+++ BootstrapFourPresenter.php
@@ -0,0 +1,128 @@
+<?php 
+
+namespace App\Services\Pagination;
+
+use Illuminate\Pagination\BootstrapThreePresenter;
+
+/*
+    Based on an article at: http://laravelista.com/laravel-custom-pagination-presenter/
+
+    The render() method accepts an Illuminate\Contracts\Pagination\Presenter instance. You can create a custom class that implements that contract and pass it to the render() method.
+    
+        
+    $users = App\User::paginate(15);
+
+    {!! with(new \App\Services\Pagination\BootstrapFourPresenter($users))->render() !!}
+
+*/
+
+class BootstrapFourPresenter extends BootstrapThreePresenter {
+
+    /**
+     * Convert the URL window into Bootstrap HTML.
+     *
+     * @return string
+     */
+    public function render()
+    {
+        if ($this->hasPages()) {
+            return sprintf(
+                '<nav><ul class="pagination">%s %s %s</ul></nav>',
+                $this->getPreviousButton(),
+                $this->getLinks(),
+                $this->getNextButton()
+            );
+        }
+
+        return '';
+    }
+
+    /**
+     * Get the previous page pagination element.
+     *
+     * @param  string  $text
+     * @return string
+     */
+    public function getPreviousButton($text = '<span aria-hidden="true"><i class="fa fa-arrow-circle-left"></i></span><span class="sr-only">Previous</span>')
+    {
+        // If the current page is less than or equal to one, it means we can't go any
+        // further back in the pages, so we will render a disabled previous button
+        // when that is the case. Otherwise, we will give it an active "status".
+        if ($this->paginator->currentPage() <= 1) {
+            return $this->getDisabledTextWrapper($text);
+        }
+
+        $url = $this->paginator->url(
+            $this->paginator->currentPage() - 1
+        );
+
+        return $this->getPageLinkWrapper($url, $text, 'prev');
+    }
+
+    /**
+     * Get the next page pagination element.
+     *
+     * @param  string  $text
+     * @return string
+     */
+    public function getNextButton($text = '<span aria-hidden="true"><i class="fa fa-arrow-circle-right"></i></span><span class="sr-only">Next</span>')
+    {
+        // If the current page is greater than or equal to the last page, it means we
+        // can't go any further into the pages, as we're already on this last page
+        // that is available, so we will make it the "next" link style disabled.
+        if (! $this->paginator->hasMorePages()) {
+            return $this->getDisabledTextWrapper($text);
+        }
+
+        $url = $this->paginator->url($this->paginator->currentPage() + 1);
+
+        return $this->getPageLinkWrapper($url, $text, 'next');
+    }
+
+    /**
+     * Get HTML wrapper for an available page link.
+     *
+     * @param  string  $url
+     * @param  int  $page
+     * @param  string|null  $rel
+     * @return string
+     */
+    protected function getAvailablePageWrapper($url, $page, $rel = null)
+    {
+        $rel = is_null($rel) ? '' : ' rel="'.$rel.'"';
+
+        return '<li class="page-item"><a class="page-link" href="'.htmlentities($url).'"'.$rel.'>'.$page.'</a></li>';
+    }
+
+    /**
+     * Get HTML wrapper for disabled text.
+     *
+     * @param  string  $text
+     * @return string
+     */
+    protected function getDisabledTextWrapper($text)
+    {
+        return '<li class="page-item disabled"><a class="page-link" href="#">'.$text.'</a></li>';
+    }
+
+    /**
+     * Get HTML wrapper for active text.
+     *
+     * @param  string  $text
+     * @return string
+     */
+    protected function getActivePageWrapper($text)
+    {
+        return '<li class="page-item active"><a class="page-link" href="#">'.$text.'</a> <span class="sr-only">(current)</span></li>';
+    }
+
+    /**
+     * Get a pagination "dot" element.
+     *
+     * @return string
+     */
+    protected function getDots()
+    {
+        return $this->getDisabledTextWrapper('&hellip;');
+    }
+}
